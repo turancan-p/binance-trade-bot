2319b3d803d53c2d8ae5551009d539c14650ad93
Turan Can Pamuk <turancan8726@outlook.com>
2022-09-27 20:10:46 +0300

    market order function added
    also get all order function added

--- talib_signals.py
+++ talib_signals.py
@@ -1,6 +1,7 @@
 from check_db_datas import get_datas
 from configs.config import SYMBOLS, INTERVAL
 import indicators.talib_indicators as indicator
+from helpers.binance_client import create_market_order, get_orders
 
 import numpy
 
@@ -23,31 +24,19 @@ def signals():
         np_lows = numpy.array(low)
         np_closes = numpy.array(close)
 
+        # STRATEGY
+
         golden_cross = False
 
         if not golden_cross and indicator.EMA(np_closes, 50) > indicator.EMA(np_closes, 200):
             golden_cross = True
             if indicator.RSI(np_closes, 14) < 30:
                 print("BUYING!", symbol)
+
+                # print(create_market_order(symbol, "BUY", 0, 0))
+
         if golden_cross and indicator.EMA(np_closes, 50) < indicator.EMA(np_closes, 200):
             if indicator.RSI(np_closes, 14) > 70:
                 print("SELLING!", symbol)
 
-        # for rsi_period in RSIPERIODS:
-        #     globals()[f'{symbol}_RSI_{rsi_period}'] = indicator.RSI(np_closes, rsi_period)
-        #     # print(f'{symbol}_RSI_{rsi_period}', globals()[f'{symbol}_RSI_{rsi_period}'])
-        #
-        # for ma_periods in MAPERIODS:
-        #     globals()[f'{symbol}_MA_{ma_periods}'] = indicator.MA(np_closes, ma_periods)
-        #     # print(f'{symbol}_MA_{ma_periods}', globals()[f'{symbol}_MA_{ma_periods}'])
-        #
-        # for ema_periods in EMAPERIODS:
-        #     globals()[f'{symbol}_EMA_{ema_periods}'] = indicator.EMA(np_closes, ema_periods)
-        #
-        # for adx_periods in ADXPERIODS:
-        #     globals()[f'{symbol}_ADX_{adx_periods}'] = indicator.ADX(np_highs, np_lows, np_closes, adx_periods)
-        #     # print(f'{symbol}_ADX_{adx_periods}', globals()[f'{symbol}_ADX_{adx_periods}'])
-        #
-        # for bbands_periods in BBANDSPERIODS:
-        #     globals()[f'{symbol}_BBAND_{bbands_periods}'] = indicator.BBANDS(np_closes, bbands_periods)
-        #     # print(f'{symbol}_BBAND_{bbands_periods}', globals()[f'{symbol}_BBAND_{bbands_periods}'])
+                # print(create_market_order(symbol, "SELL", 0, 0))
